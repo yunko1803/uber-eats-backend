fb3cb679a992fba9b078c776192d9723b78bb8e6
Yoon_Koh <bear04012@gmail.com>
2021-01-02 18:03:30 +0900

    Complete e2e testing for user login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -11,9 +11,12 @@ jest.mock('got', () => {
 });
 
 const GRAPHQL_ENDPOINT = '/graphql';
+const EMAIL = 'bear04012@gmail.com';
+const PASSWORD = '12345';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,15 +33,13 @@ describe('UserModule (e2e)', () => {
   })
 
   describe('createAccount', () => {
-    const EMAIL = "bear04012@gmail.com";
-    const password = "12345";
     it('should create account', () => {
       return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
         query: `
         mutation {
-          createAccount(input: {
-            email: "${EMAIL}",
-            password: "${password}",
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"${PASSWORD}",
             role: Client
           }) {
             ok
@@ -57,9 +58,9 @@ describe('UserModule (e2e)', () => {
       return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
         query: `
         mutation {
-          createAccount(input: {
-            email: "${EMAIL}",
-            password: "${password}",
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"${PASSWORD}",
             role: Client
           }) {
             ok
@@ -75,9 +76,62 @@ describe('UserModule (e2e)', () => {
       });
     });
   });
-  it.todo('login');
+
+  describe('login', () => {
+    it('should login with correct email and password', async () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+        mutation {
+          login(input:{
+            email:"${EMAIL}",
+            password:"${PASSWORD}",
+          }) {
+            ok
+            error
+            token
+          }
+        }
+      `,
+      })
+      .expect(200)
+      .expect(res => {
+        const { body: { data: { login }} } = res;
+        expect(login.ok).toBe(true);
+        expect(login.error).toBe(null);
+        expect(login.token).toEqual(expect.any(String));
+        jwtToken = login.token;
+      });
+    });
+
+    it('should fail log in', async () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+        mutation {
+          login(input:{
+            email:"hi",
+            password:"hey yo",
+          }) {
+            ok
+            error
+            token
+          }
+        }
+      `,
+      })
+      .expect(200)
+      .expect(res => {
+        const { body: { data: { login }} } = res;
+        expect(login.ok).toBe(false);
+        expect(login.error).toBe('User Not Found');
+        expect(login.token).toBe(null);
+      });
+    });
+  });
+
+  describe('userProfile', () => {
+    // console.log(token);
+  });
   it.todo('me');
-  it.todo('userProfile');
   it.todo('verifyEmail');
   it.todo('editProfile');
 });
