181852d51e47544734ccc1beb8db1429bd7d647d
Yoon_Koh <bear04012@gmail.com>
2021-01-02 17:36:48 +0900

    Complete e2e test for user create account

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +24,60 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  })
+
+  describe('createAccount', () => {
+    const EMAIL = "bear04012@gmail.com";
+    const password = "12345";
+    it('should create account', () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+        mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "${password}",
+            role: Client
+          }) {
+            ok
+            error
+          }
+        }
+        `
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(true);
+        expect(res.body.data.createAccount.error).toBe(null);
+      });
+    });
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+        mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "${password}",
+            role: Client
+          }) {
+            ok
+            error
+          }
+        }
+        `
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(false);
+        expect(res.body.data.createAccount.error).toBe('User Already Exists');
+      });
+    });
+  });
+  it.todo('login');
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
-  it.todo('login');
-  it.todo('editProfile');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
